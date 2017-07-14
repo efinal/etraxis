5aded360088aa5fe2621149be48d0dc97639046c
Artem Rodygin <artem.rodygin@gmail.com>
2017-07-14 15:05:44 +1200

    PostgreSQL treats NULLs as greatest values

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -13,8 +13,29 @@
 
 namespace AppBundle;
 
+use Doctrine\ORM\Query;
+use eTraxis\Doctrine\SortableNullsWalker;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        /** @var \Doctrine\ORM\EntityManagerInterface $manager */
+        $manager = $this->container->get('doctrine.orm.entity_manager');
+
+        // PostgreSQL treats NULLs as greatest values.
+        if ($this->container->getParameter('database_driver') === 'pdo_pgsql') {
+
+            $manager->getConfiguration()->setDefaultQueryHint(
+                Query::HINT_CUSTOM_OUTPUT_WALKER,
+                SortableNullsWalker::class
+            );
+        }
+    }
 }
